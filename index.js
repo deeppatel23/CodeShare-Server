65b6f459eec0df07708119bbb8de63e97723d516
Deep Patel <deeppatel@Deeps-MacBook-Air.local>
2023-06-25 12:16:46 +0530

    init

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const path = require('path');
+const { Server } = require('socket.io');
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static('codeshare-server'));
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, '', 'index.html'));
+});
+const userSocketMap = {};
+function getAllConnectedClients(roomId) {
+    // Map
+    return Array.from(io.sockets.adapter.rooms.get(roomId) || []).map(
+        (socketId) => {
+            return {
+                socketId,
+                username: userSocketMap[socketId],
+            };
+        }
+    );
+}
+
+io.on('connection', (socket) => {
+    console.log('socket connected', socket.id);
+
+    socket.on('join', ({ roomId, username }) => {
+        userSocketMap[socket.id] = username;
+        if (username !== undefined) {
+            socket.join(roomId);
+            const clients = getAllConnectedClients(roomId);
+            clients.forEach(({ socketId }) => {
+                io.to(socketId).emit('joined', {
+                    clients,
+                    username,
+                    socketId: socket.id,
+                });
+            });
+        }
+    });
+
+    socket.on('code-change', ({ roomId, code }) => {
+        socket.in(roomId).emit('code-change', { code });
+    });
+
+    socket.on('sync-code', ({ socketId, code }) => {
+        io.to(socketId).emit('code-change', { code });
+    });
+
+    socket.on('disconnecting', () => {
+        const rooms = [...socket.rooms];
+        rooms.forEach((roomId) => {
+            socket.in(roomId).emit('disconnected', {
+                socketId: socket.id,
+                username: userSocketMap[socket.id],
+            });
+        });
+        delete userSocketMap[socket.id];
+        socket.leave();
+    });
+});
+
+const PORT = process.env.PORT || 5004;
+server.listen(PORT, () => console.log(`Listening on port ${PORT}`));
